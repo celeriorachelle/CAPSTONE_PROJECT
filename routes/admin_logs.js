9ca382c82688692feac5d8db355d95d5d1e9c8b9
juzella <michaellabeneras@gmail.com>
2025-08-31 16:08:51 +0800

    tanjiro

--- routes/admin_logs.js
+++ routes/admin_logs.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('notification'); 
+  res.render('admin_logs'); 
 });
 
 module.exports = router;
